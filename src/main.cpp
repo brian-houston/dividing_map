c1764e432aa205bf2f675fad7607a8a7dd5b472e
Brian Houston <>
2023-07-13 15:51:29 -0400

    basic utility, save map as image

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,13 @@
+#include <map.h>
 #include <iostream>
-#include "test.h"
+#include <fstream>
 
 int main() {
-  hello();
+  Map m{2, 2, Map::BoundModeEnum::Clamp};
+  m.set(0, 0, 1);
+  m.set(1, 0, 2);
+  m.set(0, 1, 4);
+  m.set(1, 1, 5);
+  m.save_pbm("test.pbm", 3);
   return 0;
 }
